9dfcc7b9283daf07e5a76eb32b75b8515babf130
JD Tadlock <jt7903@gmail.com>
2022-08-26 15:42:05 -0400

    started routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,36 @@
 import { useState } from 'react';
 import BigButton from './components/BigButton';
 
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Dashboard from './pages/Dashboard';
+import NotFound from './pages/NotFound';
+
 function App() {
-  const [title, setTitle] = useState('Example Title');
-  const [animals, setAnimals] = useState(['dog', 'cat', 'elephant']);
+  // const [title, setTitle] = useState('Example Title');
+  // const [animals, setAnimals] = useState(['dog', 'cat', 'elephant']);
 
   return (
     <div>
-      <h1>{title}</h1>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+
+
+
+      {/* <h1>{title}</h1>
       {animals.map((animal, index) => <p key={index}>{animal}</p>)}
 
-      <BigButton />
+      <BigButton /> */}
     </div>
   );
 }
