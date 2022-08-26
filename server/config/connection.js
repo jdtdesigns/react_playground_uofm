3b43f73487cd770b6a38f6c4e78e398693bc95be
JD Tadlock <jt7903@gmail.com>
2022-08-26 11:12:10 -0400

    added atlas connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
+const URL = process.env.ATLAS_CONNECT_URL ? process.env.ATLAS_CONNECT_URL : 'mongodb://localhost:27017/react_playground';
 
-mongoose.connect('mongodb://localhost:27017/react_playground');
+mongoose.connect(URL);
 
 module.exports = mongoose.connection;
