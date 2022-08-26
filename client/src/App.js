853b884fbb19925f7a2953f01248a3ec7c3f9e9b
JD Tadlock <jt7903@gmail.com>
2022-08-26 15:04:33 -0400

    basic array iteration

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import { useState } from 'react';
 
 function App() {
   const [title, setTitle] = useState('Example Title');
+  const [animals, setAnimals] = useState(['dog', 'cat', 'elephant']);
 
   return (
     <div>
       <h1>{title}</h1>
+      {animals.map((animal, index) => <p key={index}>{animal}</p>)}
     </div>
   );
 }
