8ab97dc27b4930cd511976bf07515ec95d0d5d89
JD Tadlock <jt7903@gmail.com>
2022-08-29 14:06:08 -0400

    reset back to light code

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
-import BigButton from './components/BigButton';
 import StarWars from './pages/StarWars';
+import Landing from './pages/Landing';
 
 function App() {
-  const [title, setTitle] = useState('Example Title');
+  const [logo, setTitle] = useState('React Overview');
   const [animals, setAnimals] = useState(['dog', 'cat', 'elephant']);
-  const [showButton, setShowButton] = useState(true);
   const [swData, setSWData] = useState([]);
 
 
@@ -21,16 +20,16 @@ function App() {
 
   return (
     <div>
-      <h1>{title}</h1>
       {animals.map((animal, index) => <p key={index}>{animal}</p>)}
 
-      {showButton ? <BigButton grabData={grabData} title={title} /> : ''}
       <h3>Starwars Data</h3>
       {/* {swData.map((char, index) => {
         return <p key={index}>{char.name}</p>
       })} */}
 
+
       <StarWars />
+      <Landing />
 
     </div>
   );
--- client/src/components/BigButton.js
+++
@@ -1,7 +0,0 @@
-function BigButton(props) {
-  return (
-    <button onClick={props.grabData}>{props.title}</button>
-  )
-}
-
-export default BigButton;
