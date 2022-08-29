bd16bde790ce9dae2c1bcb8622e348004760bfee
JD Tadlock <jt7903@gmail.com>
2022-08-29 14:46:55 -0400

    split starwars

--- client/src/App.js
+++ client/src/App.js
@@ -5,25 +5,9 @@ import { Routes, Route } from 'react-router-dom';
 
 function App() {
   const [logo, setTitle] = useState('React Overview');
-  const [swData, setSWData] = useState([]);
-
-  const grabData = () => {
-    fetch('https://swapi.dev/api/people')
-      .then(res => res.json())
-      .then(data => {
-        setSWData(data.results);
-      });
-  };
-
-  useEffect(grabData, []);
 
   return (
     <div>
-      <h3>Starwars Data</h3>
-      {swData.map((char, index) => {
-        return <p key={index}>{char.name}</p>
-      })}
-
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/starwars" element={<StarWars />} />
--- client/src/pages/StarWars.js
+++ client/src/pages/StarWars.js
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react';
+
 function StarWars() {
+  const [swData, setSWData] = useState([]);
+
+  const grabData = () => {
+    fetch('https://swapi.dev/api/people')
+      .then(res => res.json())
+      .then(data => {
+        setSWData(data.results);
+      });
+  };
+
+  useEffect(grabData, []);
+
   return (
     <div>
-      <h1>StarWars Page</h1>
+      <h3>Starwars Data</h3>
+      {swData.map((char, index) => {
+        return <p key={index}>{char.name}</p>
+      })}
     </div>
   )
 }
